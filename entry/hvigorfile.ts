b8ad66b18e08f39e4d06f088f9db4de90c82c3c1
weitong <1758646906@qq.com>
2024-09-30 13:38:18 +0800

    初始化项目

---
+++ entry/hvigorfile.ts
@@ -0,0 +1,7 @@
+import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import { hapPlugin } from '@hadss/hmrouter-plugin';
+
+export default {
+    system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
+    plugins:[hapPlugin()]         /* Custom plugin to extend the functionality of Hvigor. */
+}
